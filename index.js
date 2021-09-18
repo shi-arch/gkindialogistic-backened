922d3568231654ef019fe1f02cfd8b9a6002d0b5
Shivram kashyap <kashyapshivram512@gmail.com>
2021-09-19 01:54:18 +0530

    commit

--- index.js
+++ index.js
@@ -7,38 +7,10 @@ const app = express()
 const bodyParser = require('body-parser')
 const path = require("path")
 
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads")
-  },
-  filename: function (req, file, cb) {
-    let fileExt = ''
-    if(file.mimetype == 'video/mp4'){
-      fileExt = '.mp4'
-    } else if(file.mimetype == 'audio/mp3'){
-      fileExt = '.mp3'
-    }  else if(file.mimetype == 'image/jpeg'){
-      fileExt = '.jpg'
-    }  else if(file.mimetype == 'image/png'){
-      fileExt = '.png'
-    }
-    cb(null, file.fieldname + "-" + Date.now() + fileExt)
-  }
-})
+var cors = require('cors');
 
-var upload = multer({
-  storage: storage,
-  fileFilter: function (req, file, cb) {
-    var filetypes = /jpeg|jpg|png|mp3|mp4/;
-    var mimetype = filetypes.test(file.mimetype);
-    var extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-    if (mimetype && extname) {
-      return cb(null, true);
-    }
-    cb("Error: File upload only supports the "
-      + "following filetypes - " + filetypes);
-  }
-});
+// use it before all route definitions
+app.use(cors({origin: 'http://localhost:3000'}));
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -46,17 +18,11 @@ app.get('/', function(req, res){
   res.send('Welcome to GKINDIALOGISTICS backened >>')
 })
 app.post('/userlist', controllers.getUserlist)
+app.post('/getallparcel', controllers.getallparcel)
+app.post('/searchparcel', controllers.searchparcel)
+app.post('/updateparcel', controllers.updateparcel)
 app.post('/adminlogin', controllers.adminLogin)
-app.post('/getnewuser', controllers.getnewuserController)
-app.post('/otpverification', controllers.otpVerification)
-app.post('/getitemList', controllers.getItemList)
-app.post('/getallcategories', controllers.getAllCategories)
-//app.post("/uploadProfilePicture", upload.array('file', 9), controllers.uploadProfilePicture)
-// app.post("/registration", controllers.registration)
-// app.post("/logout", controllers.logout)
-// app.post("/adminapproval", controllers.adminapproval)
-// app.post("/setdata", controllers.setdata)
-//app.post("/linkpostsave", controllers.linkpostsave)
+app.post('/generateparcel', controllers.generateparcel)
 
 let port =  process.env.PORT || params.port
 app.listen( port, async () => {
--- src/models/model.js
+++ src/models/model.js
@@ -1,109 +1,89 @@
 
 const connection = require('../connection/connection')
-const nodemailer = require("nodemailer");
 const constantParams = require('../constants/constant');
-var smtpTransport = require('nodemailer-smtp-transport');
 
-async function generate(n) {
-    var add = 1, max = 12 - add;
-    if (n > max) {
-        return generate(max) + generate(n - max);
-    }
-    max = Math.pow(10, n + add);
-    var min = max / 10;
-    var number = Math.floor(Math.random() * (max - min + 1)) + min;
-    return ("" + number).substring(add);
-}
-
-// var transporter = nodemailer.createTransport({
-//     host: 'smtp.gmail.com',
-//     port: 587,
-//     ignoreTLS: false,
-//     secure: false,
-//     auth: {
-//         user: constantParams.authEmail,
-//         pass: constantParams.password //replace your email and password here
-//     }
-// });
-
-var transporter = nodemailer.createTransport(smtpTransport({
-    service: 'gmail',
-    host: 'smtp.gmail.com',
-    auth: {
-        user: constantParams.authEmail,
-        pass: constantParams.password //replace your email and password here
-    }
-}));
-
-async function getnewuserModel(body) {
-    try {
-        let dbConn = connection.getDb()
-        const collection = await dbConn.collection("users");
-        let otp = await generate(6)
-        if (body.email) {
-            const isEmailExists = await collection.findOne({ email: body.email })
-            if (isEmailExists == null) {
-                let response = await collection.insertOne({ email: body.email, isEmailVerified: false, otp: otp })
-                if (response !== null) {
-                    var mailOptions = {
-                        from: 'testshivramkashyap512@gmail.com',
-                        to: body.email,
-                        subject: constantParams.subject,
-                        html: "<h3>Please use this otp to verify its you</h3> : " + "<span>" + otp + "</span>"
-                    };
-                    let result = await transporter.sendMail(mailOptions);
-                    if (result && result.response) {
-                        return { data: {}, message: constantParams.otpSent, status: true }
-                    }
-                } else {
-                    return { data: {}, message: constantParams.authFail, status: false }
-                }
-            } else {
-                return { data: {}, message: 'email already exists', status: false }
-            }
-        } else {
-            return { data: {}, message: constantParams.authFail, status: false }
-        }
-    } catch (err) {
-        console.log(err, '>>>>>>>>>>>>>>>>>>>>');
-    }
-
-}
-
-async function otpVerification(otp) {
+async function getallparcel(body) {
     let dbConn = connection.getDb()
-    const collection = await dbConn.collection("users");
+    const collection = await dbConn.collection("parcelData");
+    const totalCount = await collection.aggregate([
+        {
+            $group: {
+                _id: null,
+                count: {$sum: 1}
+            }
+        }
+    ]).toArray() 
+    let response = await collection.aggregate([
+        { $skip : body.skip }, {$limit: body.limit }
+    ]).toArray() 
+    console.log(response,'sssssssssssssssssssss')  
+    if (response && response.length) {
+        return { data: {count : totalCount[0].count, data: response}, message: "Here is your all parcels", status: true }
+    } else {
+        return { data: {}, message: "No parcel found", status: false }
+    }
+}
+
+async function generateparcel(body) {
+    let dbConn = connection.getDb()
+    const collection = await dbConn.collection("parcelData");
+    const response = await collection.aggregate([
+        {
+            $group: {
+                _id: null,
+                maxQuantity: {$max: "$parcelNumber"}
+            }
+        }
+    ]).toArray()   
+    if (response && response.length) {
+        body.parcelNumber = response[0].maxQuantity + 1
+    } else {
+        body.parcelNumber = 1     
+    }
+    let result = await collection.insertOne(body)
+    if (result !== null) {
+        return { message: "Parcel number generated successfully", status: true }
+    } else {
+        return { message: "something went wrong", status: false }
+    }
+
+}
+
+
+async function searchparcel(body) {
+    let dbConn = connection.getDb()
+    const collection = await dbConn.collection("parcelData"); 
+    let obj = {} 
+    if(body.type == "Enter Mobile Number"){
+        obj.contact = body.contact
+    }
+    
+    if(body.type == "Enter Parcel Id"){
+        obj.parcelNumber = JSON.parse(body.parcelNumber.charAt(4))
+    }  
+    const response = await collection.find(obj).toArray()
+    if (response && response.length) {
+        return { data: response, message: "Parcel details found successfully", status: true }
+    } else {
+        return { data: {}, message: "Parcel not found", status: false }
+    }
+}
+
+async function updateparcel(body) {
+    let dbConn = connection.getDb()
+    const collection = await dbConn.collection("parcelData");
     let response = await collection.findOneAndUpdate(
-        { otp: otp },
-        { $set: { isEmailVerified: true } }
+        { parcelNumber: body.parcelNumber },
+        { $set: { status: body.status } }
     )
     if (response && response.value !== null) {
-        return { data: response.value, message: constantParams.succesMsg, status: true }
+        return { data: response.value, message: "Parcel Status is updated successfully", status: true }
     } else {
-        return { data: {}, message: constantParams.otpCheck, status: false }
+        return { data: {}, message: "Something went wrong", status: false }
     }
 }
 
-async function getItemList() {
-    let dbConn = connection.getDb()
-    const collection = await dbConn.collection("product_list");
-    let response = await collection.find({}).toArray()
-    if (response && response.length) {
-        return { data: response, message: constantParams.requestMsg, status: true }
-    } else {
-        return { data: {}, message: constantParams.otpCheck, status: false }
-    }
-}
-async function getAllCategories() {
-    let dbConn = connection.getDb()
-    const collection = await dbConn.collection("category_list");
-    let response = await collection.find({}).toArray()
-    if (response && response.length) {
-        return { data: response, message: constantParams.requestMsg, status: true }
-    } else {
-        return { data: {}, message: constantParams.otpCheck, status: false }
-    }
-}
+
 
 
 async function getUserlist() {
@@ -128,164 +108,11 @@ async function adminLogin(body) {
     }
 }
 
-
-// async function registration(body) {
-//     let dbConn = connection.getDb()
-//     const collection = await dbConn.collection("userData");
-//     let obj = {
-//         email: body.email
-//     }
-//     if (body && body.admin) {
-//         obj.admin = true
-//     }
-//     let response = await collection.findOne(obj)
-//     if (response == null) {
-//         let otp = await generate(6)
-//         obj = {
-//             token: '',
-//             otp: otp,
-//             name: body.name,
-//             dob: body.dob,
-//             password: body.password,
-//             isEmailVerified: false,
-//             email: body.email
-//         }
-//         if (body && body.admin) {
-//             obj.admin = true
-//             obj.isApproved = false
-//         }
-//         let response = await collection.insertOne(obj)
-//         if (response !== null) {
-//             var mailOptions = {
-//                 from: 'kashyapshivram512@gmail.com',
-//                 to: body.email,
-//                 subject: constantParams.subject,
-//                 html: "<h3>Please use this otp to login</h3> : " + "<span>" + otp + "</span>"
-//             };
-//             let result = await transporter.sendMail(mailOptions);
-//             if (result && result.response) {
-//                 return { data: {}, message: constantParams.checkOtp, status: true }
-//             } else {
-//                 return { data: response.value, message: constantParams.otpNotSent, status: false }
-//             }
-//         } else {
-//             return { data: response, message: constantParams.dataNotSave, status: false }
-//         }
-//     } else {
-//         return { data: {}, message: constantParams.existMsg, status: false }
-//     }
-
-
-// }
-// async function logout(body) {
-//     let dbConn = connection.getDb()
-//     const collection = await dbConn.collection("userData");
-//     let updateResponse = await collection.findOneAndUpdate(
-//         { email: body.email },
-//         { $set: { token: '', otp: '' } }
-//     )
-//     if (updateResponse !== null) {
-//         return { data: {}, message: constantParams.logOut, status: true }
-//     } else {
-//         return { data: response, message: constantParams.wrongEmail, status: false }
-//     }
-// }
-
-// async function adminapproval(body) {
-//     let dbConn = connection.getDb()
-//     const collection = await dbConn.collection("userData");
-//     let updateResponse = await collection.findOneAndUpdate(
-//         { email: body.email, isAdmin: true },
-//         { $set: { isApproved: true } }
-//     )
-//     if (updateResponse.value !== null) {
-//         var mailOptions = {
-//             from: 'kashyapshivram512@gmail.com',
-//             to: body.email,
-//             subject: 'Admin approval from super admin',
-//             html: "<h3>Congratulations...! You are approved as admin</h3>"
-//         };
-//         let result = await transporter.sendMail(mailOptions);
-//         if (result && result.response) {
-//             return { data: {}, message: 'Admin approved', status: true }
-//         } else {
-//             return { data: {}, message: constantParams.mailNotSent, status: false }
-//         }
-//     } else {
-//         return { data: response, message: constantParams.wrongEmail, status: false }
-//     }
-// }
-// async function uploadFileSetToDb(body) {
-//     let dbConn = connection.getDb()
-//     const collection = await dbConn.collection("PostContentData");
-//     let result = await collection.insertOne(body)
-//     if (result !== null) {
-//         return { data: {}, message: constantParams.postMsg, status: true }
-//     } else {
-//         return { data: {}, message: constantParams.notSave, status: false }
-//     }
-// }
-// async function linkpostsave(body, email) {
-//     let dbConn = connection.getDb()
-//     const collection = await dbConn.collection("PostContentData");
-//     const userCollection = await dbConn.collection("userData");
-//     let response = await userCollection.findOne({ email: email, isEmailVerified: true })
-//     if (response !== null) {
-//         if (response.superAdmin) {
-//             body.isApproved = true
-//         } else if (response.admin) {
-//             body.isApproved = false
-//         }
-//         let result = await collection.insertOne(body)
-//         if (result !== null) {
-//             return { data: {}, message: constantParams.postMsg, status: true }
-//         } else {
-//             return { data: {}, message: constantParams.notSave, status: false }
-//         }
-//     } else {
-//         return { data: {}, message: constantParams.mailNotExist, status: false }
-//     }
-// }
-// async function setdata(body) {
-//     let dbConn = connection.getDb()
-//     const collection = await dbConn.collection("PostContentData");
-//     const userCollection = await dbConn.collection("userData");
-//     let response = await userCollection.findOne({ email: body.email, isEmailVerified: true })
-//     if (response !== null) {
-//         let obj = {
-//             postTitle: body.postTitle,
-//             postCategory: body.postCategory
-//         }
-//         if (response.superAdmin) {
-//             obj.isApproved = true
-//         } else if (response.admin) {
-//             obj.isApproved = false
-//         }
-//         let result = await collection.findOneAndUpdate(
-//             { "fileDetails.filename": body.filename },
-//             { $set: obj }
-//         )
-//         if (result && result.value !== null) {
-//             return { data: {}, message: constantParams.postMsg, status: true }
-//         } else {
-//             return { data: {}, message: constantParams.notSave, status: false }
-//         }
-//     } else {
-//         return { data: {}, message: constantParams.mailNotExist, status: false }
-//     }
-// }
-
 module.exports = {
-    getnewuserModel: getnewuserModel,
-    otpVerification: otpVerification,
-    getItemList: getItemList,
-    getAllCategories: getAllCategories,
     getUserlist: getUserlist,
-    adminLogin: adminLogin
-    // registration: registration,
-    // logout: logout,
-    // adminapproval: adminapproval,
-    // setdata: setdata,
-    // uploadFileSetToDb: uploadFileSetToDb,
-    // linkpostsave: linkpostsave
+    adminLogin: adminLogin,
+    generateparcel: generateparcel,
+    searchparcel: searchparcel,
+    getallparcel: getallparcel,
+    updateparcel: updateparcel
 }
